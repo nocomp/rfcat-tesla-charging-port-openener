f044fe34691bc66403ea79980951e1b99c3b363d
nocomp <nocomp@gmail.com>
2022-05-31 15:53:57 +0200

    Update tesla-rfcat.py

--- tesla-rfcat.py
+++ tesla-rfcat.py
@@ -16,8 +16,7 @@ d.setFreq(433920000) # Freq for EU and rest of the world
 #d.setFreq(315000000) #Freq for USA
 d.setMdmModulation(MOD_ASK_OOK)
 d.setMdmDRate(2500)
-d.setAmpMode(RF_TX_POWER_AMPLIFIER_ACTION_ON)
-d.setMaxPower()
+d.setAmpMode(1)
 print ("Transmition Starting")
 d.RFxmit(b'\x15\x55\x55\x51\x59\x4C\xB5\x55\x52\xD5\x4B\x4A\xD3\x4C\xAB\x4B\x15\x94\xCB\x33\x33\x2D\x54\xB4\x56\x9A\x65\x5A\x48\xAC\xC6\x59\x99\x99\x69\xA5\xB2\xB4\xD4\x2A\xD2\x80'*5)
 
